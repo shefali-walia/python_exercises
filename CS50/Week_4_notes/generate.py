6f1e748b7c55902b44ce0d2fcc69cef382bcc80f
shefali-walia <shefali.walia.04@gmail.com>
2026-04-17 09:09:02 +0400

    Added Week_4_notes

---
+++ CS50/Week_4_notes/generate.py
@@ -0,0 +1,12 @@
+import random 
+
+# coin = random.choice(["heads", "tails"])
+# print(coin)
+
+# number = random.randint(1,10)
+# print(number)
+
+cards = ["jack", "queen", "king", "ace" ]
+random.shuffle(cards)
+for card in cards:
+    print(card)
